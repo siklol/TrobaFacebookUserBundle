6cb5363b69a337c8a40f8ce8932384e502fdd732
siklol <mail@veitosiander.de>
2015-01-29 01:22:10 +0100

    added missing dependencies

--- Listener/FacebookSessionListener.php
+++ Listener/FacebookSessionListener.php
@@ -2,17 +2,31 @@
 namespace SikIndustries\Bundles\FacebookBundle\Listener;
 
 use Facebook\FacebookSession;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class FacebookSessionListener
 {
-    public function __construct($appId, $appSecret)
+    protected $appId;
+    protected $appSecret;
+
+    /**
+     * @var Session
+     */
+    protected $session;
+    public function __construct($appId, $appSecret, Session $session)
     {
         $this->appId = $appId;
         $this->appSecret = $appSecret;
+        $this->session = $session;
     }
+
+    /**
+     * @param GetResponseEvent $event
+     */
     public function onKernelRequest(GetResponseEvent $event)
     {
+        $this->session->start();
         FacebookSession::setDefaultApplication($this->appId, $this->appSecret);
     }
 }
